3a85466479d0fc5681bd82d25deddaf7e1e059c0
hum55 <humberto.ramirez@tecdesoftware.edu.mx>
2026-05-13 17:55:19 -0600

    Resolver conflictos - Agregar juegos

---
+++ Enemigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ahorcado
+{
+    public class Enemigo
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Vida { get; set; }
+
+        public Enemigo(int x, int y, int vida)
+        {
+            X = x;
+            Y = y;
+            Vida = vida;
+        }
+    }
+}
